e52634e12da223a3c236328a6c558266781f3fa3
batuser11 <167933883+batuser11@users.noreply.github.com>
2024-04-24 03:44:02 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,18 @@
+import telebot
+from decouple import config
+
+BOT_TOKEN = config('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=["start","help"])
+def welcome(message):
+    bot.send-message(message.chat.id,"welcome to My Bot")
+    #answer all msg
+    def isMSg(message): 
+        return True
+    
+    @bot.message_handler(func=isMSg)        
+    def reply(messagr):
+        bot.reply_to(message,"that not a command")
+bot.polling()
